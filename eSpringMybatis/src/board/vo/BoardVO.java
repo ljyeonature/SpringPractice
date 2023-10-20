62ace8d36e637eda9afdb0e2e5dd3978ac04434d
ict0309 <ljyeon131@gmail.com>
2023-10-20 15:02:23 +0900

    mybatis 연습

---
+++ eSpringMybatis/src/board/vo/BoardVO.java
@@ -0,0 +1,20 @@
+package board.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+//VO(Value Object)
+@Data
+public class BoardVO{
+
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate;
+	private int cnt;
+
+
+	
+}
