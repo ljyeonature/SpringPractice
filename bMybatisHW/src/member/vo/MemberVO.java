853f6c0430e628ac2e5a23ae8e56c6c59fab9ea0
ict0309 <ljyeon131@gmail.com>
2023-10-20 15:01:13 +0900

    과제2(10/20)

---
+++ bMybatisHW/src/member/vo/MemberVO.java
@@ -0,0 +1,15 @@
+package member.vo;
+
+import lombok.Data;
+
+//VO(Value Object)
+@Data
+public class MemberVO {
+
+	private String userid;
+	private String userpass;
+	private String username;
+
+
+	
+}
