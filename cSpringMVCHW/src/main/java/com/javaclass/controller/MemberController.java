2cfd1e73ee794730e599269d878e0d8d01ba4eaf
ict0309 <ljyeon131@gmail.com>
2023-10-24 15:46:29 +0900

    과제3(회원가입)/3-1(로그인)

---
+++ cSpringMVCHW/src/main/java/com/javaclass/controller/MemberController.java
@@ -0,0 +1,48 @@
+package com.javaclass.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javaclass.domain.MemberVO;
+import com.javaclass.service.MemberService;
+
+// ### (1) 해당 어노테이션 지정
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+	// ### (2) 해당 어노테이션 지정
+	@Autowired
+	private MemberService memberServiceImpl;
+	
+	@RequestMapping("/{step}.do")
+	public String viewPage(@PathVariable String step) {
+		return step;
+	}
+	
+	// ### (3) 해당 어노테이션 지정
+	@RequestMapping("/insertMember.do")
+	public String insertMember( MemberVO vo ) {
+		memberServiceImpl.insertMember(vo);
+		return "insertMember";
+	}
+	
+	// ### (4) 해당 어노테이션 지정
+	@RequestMapping("/login.do")
+	public String login(MemberVO vo, HttpSession session) {
+		MemberVO result = memberServiceImpl.loginCheck(vo);
+		if(result==null) {
+			return "loginForm";
+		}else {
+			// (5) 로그인 성공한 사용자이름을 세션에 저장
+			session.setAttribute("logname", vo.getUserid());
+			
+			return "loginConfirm";
+		}
+	}
+	
+}
---
+++ cSpringMVCHW/src/main/java/com/javaclass/domain/MemberVO.java
@@ -0,0 +1,12 @@
+package com.javaclass.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	
+	private String userid;
+	private String userpass;
+	private String username;
+
+}
